aa35f4b40dbb0275d47dcd84a1c2fc05bd05f2ab
Aryan Shobuj <83237247+AriyanShobuj@users.noreply.github.com>
2021-05-05 22:02:25 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+// slider====
+    $('.luxury__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        fade:true,
+        dots:true,
+        autoplaySpeed: 2000,
+        arrows:false,
+      });
+
+
+})
